ec2b6d7a9fda6dd566e251ae5e487f5c39320edc
Rohitbabu18 <18rohitbabu@gmail.com>
2022-11-11 23:54:47 +0530

    'first'

--- src/Utils/Colors.js
+++ src/Utils/Colors.js
@@ -1,13 +1,15 @@
 const Colors = {
     statusBar: 'hsl(0,0%,90%)',
     appBack: 'hsl(0,0%,90%)',
+    button: "#d55556",//'#b5734c' #cf5953
+
     statusbarTans: 'rgbs(52,52,52,0.8)',
     primary: '#1292B4',
     lighter: '#F3F3F3',
     light: '#DAE1E7',
     dark: '#444',
     darker: '#222',
-    black: '#000000',
+    black: '#19191b',
     blue: 'blue',
     lightBlue: '#B0E0E6',
     BlueSecond: '#0096FF',
@@ -15,10 +17,10 @@ const Colors = {
     charcoal: '#474747',
     gray: '#7D7D7D',
     lightishgray: '#9D9D9D',
-    lightgray: '#D6D6D6',
+    lightgray: '#D6D6D6',// #f4f4f4
     smoke: '#EEEEEE',
     white: '#FFFFFF',
-    backWhite: '#f1f1f1',
+    backWhite: '#fdfdfd',
     ypsDark: '#47546E',
     yps: '#637599',
     ypsLight: '#7B92BF',
@@ -29,5 +31,6 @@ const Colors = {
     orange: 'orange',
     lightGreen: 'lightgreen',
     transparent: 'transparent',
+
 };
 export default Colors;
